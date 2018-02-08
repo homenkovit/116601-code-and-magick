b64338a794c471135f7e985c6dc611ebc6c46a79
Vitaly <homenkovit@gmail.com>
2018-02-09 01:39:17 +0300

    Написал функцию выбора рандомного элемента из массива и добавил обработчики событий на открыте/зыкрытие попапа, изменение цвета

--- js/setup.js
+++ js/setup.js
@@ -1,28 +1,42 @@
 'use strict';
 
 var setupWindow = document.querySelector('.setup');
+var openSetupWindowButton = document.querySelector('.setup-open');
+var closeSetupWindowButton = setupWindow.querySelector('.setup-close');
+var setupUserNameField = setupWindow.querySelector('.setup-user-name');
+var setupWizard = setupWindow.querySelector('.setup-wizard');
+var setupWizardCoat = setupWizard.querySelector('.wizard-coat');
+var setupWizardEyes = setupWizard.querySelector('.wizard-eyes');
+var setupWizardFireball = setupWindow.querySelector('.setup-fireball-wrap');
 var similarWizardBlock = setupWindow.querySelector('.setup-similar');
 var similarWizardList = setupWindow.querySelector('.setup-similar-list');
 var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
+
+var ESC_KEYCODE = 27;
+var ENTER_KEYCODE = 13;
 
 var FIRST_NAMES_ARRAY = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var SECOND_NAMES_ARRAY = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var COAT_COLORS_ARRAY = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var EYES_COLORS_ARRAY = ['black', 'red', 'blue', 'yellow', 'green'];
+var FIREBALL_COLORS_ARRAY = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
 var wizardsAmount = 4;
 var wizards = [];
 
+var getRandomElementOfArray = function (array) {
+  var randomIndex = Math.floor(Math.random() * array.length);
+  var randomElement = array[randomIndex];
+
+  return randomElement;
+};
+
 var createWizard = function (firstNamesArray, secondNamesArray, coatColorsArray, eyesColorsArray) {
-  var randomFirstNameIndex = Math.floor(Math.random() * firstNamesArray.length);
-  var randomSecondNameIndex = Math.floor(Math.random() * secondNamesArray.length);
-  var randomCoatColorIndex = Math.floor(Math.random() * coatColorsArray.length);
-  var randomEyesColorIndex = Math.floor(Math.random() * eyesColorsArray.length);
-  var wizardName = (firstNamesArray[randomFirstNameIndex] + ' ' + secondNamesArray[randomSecondNameIndex]);
+  var wizardName = (getRandomElementOfArray(firstNamesArray) + ' ' + getRandomElementOfArray(secondNamesArray));
 
   var wizard = {
     name: wizardName,
-    coatColor: coatColorsArray[randomCoatColorIndex],
-    eyesColor: eyesColorsArray[randomEyesColorIndex]
+    coatColor: getRandomElementOfArray(coatColorsArray),
+    eyesColor: getRandomElementOfArray(eyesColorsArray)
   };
 
   return wizard;
@@ -51,4 +65,45 @@ for (var j = 0; j < wizards.length; j++) {
 similarWizardList.appendChild(fragment);
 
 similarWizardBlock.classList.remove('hidden');
-setupWindow.classList.remove('hidden');
+
+var setupWindowEscHandler = function (evt) {
+  if (evt.keyCode === ESC_KEYCODE) {
+    closeSetupWindowHandler();
+  }
+};
+var openSetupWindowHandler = function () {
+  setupWindow.classList.remove('hidden');
+  document.addEventListener('keydown', setupWindowEscHandler);
+};
+var closeSetupWindowHandler = function () {
+  setupWindow.classList.add('hidden');
+  document.removeEventListener('keydown', setupWindowEscHandler);
+};
+
+openSetupWindowButton.addEventListener('click', openSetupWindowHandler);
+closeSetupWindowButton.addEventListener('click', closeSetupWindowHandler);
+
+openSetupWindowButton.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    openSetupWindowHandler();
+  }
+});
+closeSetupWindowButton.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    closeSetupWindowHandler();
+  }
+});
+
+setupUserNameField.addEventListener('focus', function () {
+  document.removeEventListener('keydown', setupWindowEscHandler);
+});
+
+setupWizardCoat.addEventListener('click', function (evt) {
+  evt.target.style.fill = getRandomElementOfArray(COAT_COLORS_ARRAY);
+});
+setupWizardEyes.addEventListener('click', function (evt) {
+  evt.target.style.fill = getRandomElementOfArray(EYES_COLORS_ARRAY);
+});
+setupWizardFireball.addEventListener('click', function (evt) {
+  evt.target.style.backgroundColor = getRandomElementOfArray(FIREBALL_COLORS_ARRAY);
+});
