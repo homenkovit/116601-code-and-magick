a9d8dcc40ab0351f3ff180a6b86e51b7125bcc78
Vitaly <homenkovit@gmail.com>
2018-02-09 22:49:10 +0300

    Вынес константы наверх, переименовал функции и вынес в отдедльную функцию смену fill

--- js/setup.js
+++ js/setup.js
@@ -1,5 +1,16 @@
 'use strict';
 
+var ESC_KEYCODE = 27;
+var ENTER_KEYCODE = 13;
+
+var FIRST_NAMES_ARRAY = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+var SECOND_NAMES_ARRAY = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+var COAT_COLORS_ARRAY = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var EYES_COLORS_ARRAY = ['black', 'red', 'blue', 'yellow', 'green'];
+var FIREBALL_COLORS_ARRAY = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
+
+var wizardsAmount = 4;
+var wizards = [];
 var setupWindow = document.querySelector('.setup');
 var openSetupWindowButton = document.querySelector('.setup-open');
 var closeSetupWindowButton = setupWindow.querySelector('.setup-close');
@@ -11,17 +22,6 @@ var setupWizardFireball = setupWindow.querySelector('.setup-fireball-wrap');
 var similarWizardBlock = setupWindow.querySelector('.setup-similar');
 var similarWizardList = setupWindow.querySelector('.setup-similar-list');
 var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
-
-var ESC_KEYCODE = 27;
-var ENTER_KEYCODE = 13;
-
-var FIRST_NAMES_ARRAY = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
-var SECOND_NAMES_ARRAY = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-var COAT_COLORS_ARRAY = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
-var EYES_COLORS_ARRAY = ['black', 'red', 'blue', 'yellow', 'green'];
-var FIREBALL_COLORS_ARRAY = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
-var wizardsAmount = 4;
-var wizards = [];
 
 var getRandomElementOfArray = function (array) {
   var randomIndex = Math.floor(Math.random() * array.length);
@@ -68,29 +68,29 @@ similarWizardBlock.classList.remove('hidden');
 
 var setupWindowEscHandler = function (evt) {
   if (evt.keyCode === ESC_KEYCODE) {
-    closeSetupWindowHandler();
+    closeSetupWindow();
   }
 };
-var openSetupWindowHandler = function () {
+var openSetupWindow = function () {
   setupWindow.classList.remove('hidden');
   document.addEventListener('keydown', setupWindowEscHandler);
 };
-var closeSetupWindowHandler = function () {
+var closeSetupWindow = function () {
   setupWindow.classList.add('hidden');
   document.removeEventListener('keydown', setupWindowEscHandler);
 };
 
-openSetupWindowButton.addEventListener('click', openSetupWindowHandler);
-closeSetupWindowButton.addEventListener('click', closeSetupWindowHandler);
+openSetupWindowButton.addEventListener('click', openSetupWindow);
+closeSetupWindowButton.addEventListener('click', closeSetupWindow);
 
 openSetupWindowButton.addEventListener('keydown', function (evt) {
   if (evt.keyCode === ENTER_KEYCODE) {
-    openSetupWindowHandler();
+    openSetupWindow();
   }
 });
 closeSetupWindowButton.addEventListener('keydown', function (evt) {
   if (evt.keyCode === ENTER_KEYCODE) {
-    closeSetupWindowHandler();
+    closeSetupWindow();
   }
 });
 
@@ -98,11 +98,14 @@ setupUserNameField.addEventListener('focus', function () {
   document.removeEventListener('keydown', setupWindowEscHandler);
 });
 
+var setFill = function (evt, array) {
+  evt.target.style.fill = getRandomElementOfArray(array);
+};
 setupWizardCoat.addEventListener('click', function (evt) {
-  evt.target.style.fill = getRandomElementOfArray(COAT_COLORS_ARRAY);
+  setFill(evt, COAT_COLORS_ARRAY);
 });
 setupWizardEyes.addEventListener('click', function (evt) {
-  evt.target.style.fill = getRandomElementOfArray(EYES_COLORS_ARRAY);
+  setFill(evt, EYES_COLORS_ARRAY);
 });
 setupWizardFireball.addEventListener('click', function (evt) {
   evt.target.style.backgroundColor = getRandomElementOfArray(FIREBALL_COLORS_ARRAY);
